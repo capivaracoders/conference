eb2510f306f0d525f7333fe681931b09e792a263
William S Rodriguez <william.rodriguez@arctouch.com>
2020-04-08 22:48:57 -0300

    added online event

--- capiconf/src/app/app-routing.module.ts
+++ capiconf/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CocComponent } from './conferences/shared/coc/coc.component';
 import { IncluaTodaPessoaComponent } from './campaigns/inclua-toda-pessoa/inclua-toda-pessoa.component';
+import { OnlineComponent } from './conferences/editions/20online/online/online.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '2020/inicio', pathMatch: 'full' },
+  //{ path: '', redirectTo: '2020/inicio', pathMatch: 'full' },
+  { path: '', redirectTo: 'online', pathMatch: 'full' },
   {
     path: '2020',
     loadChildren: () =>
@@ -19,6 +21,7 @@ const routes: Routes = [
         m => m.Capybara2019Module,
       ),
   },
+  { path: 'online', component: OnlineComponent },
   { path: 'inclua-toda-pessoa', component: IncluaTodaPessoaComponent },
   { path: 'coc', component: CocComponent },
   { path: '**', redirectTo: '2020/inicio' },

5c1b32491455dd2fdae2751a0bcb54dfe6d16b07
William S. Rodriguez <williamsrz@outlook.com>
2020-10-12 16:50:14 -0300

    🚧 register sponsor module

--- capiconf/src/app/app.module.ts
+++ capiconf/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { CocComponent } from './conferences/shared/coc/coc.component';
 import { IncluaTodaPessoaComponent } from './campaigns/inclua-toda-pessoa/inclua-toda-pessoa.component';
 import { Online2020NovComponent } from './conferences/editions/20online-nov/online/online2020-nov.component';
 import { AdiamentoComponent } from './conferences/editions/adiamento/adiamento.component';
+import { Capi20SponsorsComponent } from './conferences/editions/20/capi20-sponsors/capi20-sponsors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CocComponent,
     IncluaTodaPessoaComponent,
+    Capi20SponsorsComponent,
     Online2020NovComponent,
     AdiamentoComponent
   ],
